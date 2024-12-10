afc9bb673f45a15b26d6daf6bfd238d19d2b1747
A Beetle <abeetle15@gmail.com>
2024-12-10 18:42:45 -0500

    Legacy frontend.
    I am going to start the frontend over again with best practices.

--- frontend/src/components/SecretsDisplay.jsx
+++
@@ -1,9 +0,0 @@
-import IndividualSecret from "./IndividualSecret";
-
-export default function SecretsDisplay({ secret }) {
-  return (
-    <div id="display-main-cont">
-      <IndividualSecret secret={secret} />
-    </div>
-  );
-}
